cb6b1e581e64f0f00f959b4d72e5d74b820fe571
Doğuş  Kaynak <doguskaynak@Doguss-Mac-mini.local>
2021-03-05 00:02:32 +0300

    first commit

---
+++ newby/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newby
+//
+//  Created by Doğuş  Kaynak on 4.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
